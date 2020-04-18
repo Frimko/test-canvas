03aedd3a9262666e30d4156555ac3ab907748d4f
Sergey Samokhvalov <ccc-car@yandex.ru>
2020-04-18 19:35:49 +0800

    fix

---
+++ build/precache-manifest.8d9aeb1d5814152c0ba89c12099e1117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125b5ad580140dca3117a89053a7dc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be695ac41ee0daf80c14",
+    "url": "/static/css/main.70d642c2.chunk.css"
+  },
+  {
+    "revision": "cfd95633bd8211efb236",
+    "url": "/static/js/2.ae75f8f6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ae75f8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be695ac41ee0daf80c14",
+    "url": "/static/js/main.466e5b7e.chunk.js"
+  },
+  {
+    "revision": "1ded915e452135905175",
+    "url": "/static/js/runtime-main.93341991.js"
+  }
+]);
